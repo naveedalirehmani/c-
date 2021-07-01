96b2550a8ad9415f285e74f9def913c8a4d2ded3
naveedali <naveedalirehmani321@gmail.com>
2021-07-01 22:05:24 +0500

    assingment mammals

---
+++ mammal.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Mammal{
+    public:
+        void mammalMethod(){
+            cout<<"I am a Mammal"<<endl;
+        }
+};
+class MarineAnimal{
+    public:
+        void marineAnimalMethod(){
+            cout<<"I am a MarineAnimal"<<endl;
+        }
+};
+class BlueWhale:public MarineAnimal,public Mammal{
+    public:
+        void blueWhaleMethod(){
+            cout<<"I am a Blue Whale I am both Mammal and Marine Animal"<<endl;
+        };
+};
+
+int main (){
+    Mammal mammalObject;
+    MarineAnimal marineAnimalObject;
+    BlueWhale blueWhaleObject;
+    mammalObject.mammalMethod();
+    marineAnimalObject.marineAnimalMethod();
+    blueWhaleObject.blueWhaleMethod();
+    system("pause");
+    return 0;
+}
